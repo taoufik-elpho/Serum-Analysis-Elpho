61e5fd9bf24ec10e2694e30b306f9ffcac67c86f
TAOUFIK.BENSELLAK <bensellak.taoufik@gmail.com>
2020-01-19 13:54:19 +0100

    Update CombiningEngines.R

--- R Scripts/CombiningEngines.R
+++ R Scripts/CombiningEngines.R
@@ -25,4 +25,4 @@ fwrite(proteinscount,paste0("Tables/Proteins_Count_Per_Sample_Engine_FDR_Protein
 controlcase=fread("control-case.csv")
 controlcase=controlcase[controlcase$Control%in%names(enginesidentification),]
 controlcase=controlcase[controlcase$Case%in%names(enginesidentification),]
-source("makeupsetscontrolvscase.R")
+source("MakeUpsetsControlvsCaseFigures.R")
